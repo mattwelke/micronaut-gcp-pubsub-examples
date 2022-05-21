3b8bd450b4fd1d2848445d5163f52e94bb64f2f9
Matt Welke <mattwelke@gmail.com>
2022-05-21 14:37:44 -0400

    Add pipeline app. TODO: solve shutdown order problem

---
+++ pubsub-pipeline-app/src/main/java/pubsub/pipeline/app/ValidMessagePublisher.java
@@ -0,0 +1,15 @@
+package pubsub.pipeline.app;
+
+import io.micronaut.gcp.pubsub.annotation.PubSubClient;
+import io.micronaut.gcp.pubsub.annotation.Topic;
+
+@PubSubClient
+public interface ValidMessagePublisher {
+
+    /**
+     * Publishes a validated message.
+     * @param message The validated message.
+     */
+    @Topic("messages-valid")
+    void send(Message message);
+}
